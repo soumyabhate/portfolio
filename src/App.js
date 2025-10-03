5854f39abefdf3c01adab8ae1dda1471909bd38b
Soumya Bhate <56678035+soumyabhate@users.noreply.github.com>
2025-10-03 17:34:31 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import About from './components/About';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
